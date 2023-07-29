de7d26b8aa670886d30315f51f3a48d9ef997b3a
woohm402 <woohm404@gmail.com>
2023-07-29 14:58:59 +0900

    setup drawer

--- friends/src/app/App.tsx
+++ friends/src/app/App.tsx
@@ -1,12 +1,20 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { MainScreen } from './screens/MainScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import 'react-native-gesture-handler';
 
 const queryClient = new QueryClient();
+const Drawer = createDrawerNavigator();
 
 export const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
-      <MainScreen />
+      <NavigationContainer>
+        <Drawer.Navigator>
+          <Drawer.Screen name="Main" component={MainScreen} />
+        </Drawer.Navigator>
+      </NavigationContainer>
     </QueryClientProvider>
   );
 };
