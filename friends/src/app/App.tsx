59751ddc4361851652a707e08ff919e348d54295
Hyunmin Woo (Hanul) <woohm404@gmail.com>
2023-08-08 00:33:19 +0900

    ✨ setup drawer (#15)

--- friends/src/app/App.tsx
+++ friends/src/app/App.tsx
@@ -1,12 +1,17 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { MainScreen } from './screens/MainScreen';
+import { NavigationContainer } from '@react-navigation/native';
+
+import 'react-native-gesture-handler';
 
 const queryClient = new QueryClient();
 
 export const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
-      <MainScreen />
+      <NavigationContainer>
+        <MainScreen />
+      </NavigationContainer>
     </QueryClientProvider>
   );
 };
