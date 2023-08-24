e591b10e7744d3d862de4747809902c9b7674d87
woohm402 <woohm404@gmail.com>
2023-08-24 22:43:15 +0900

    SafeAreaView 적용

--- friends/src/app/App.tsx
+++ friends/src/app/App.tsx
@@ -2,17 +2,28 @@ import 'react-native-gesture-handler';
 
 import { NavigationContainer } from '@react-navigation/native';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { StyleSheet } from 'react-native';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
+import { useThemeContext } from './contexts/ThemeContext';
 import { MainScreen } from './screens/MainScreen';
 
 const queryClient = new QueryClient();
 
 export const App = () => {
+  const backgroundColor = useThemeContext((data) => data.color.bg.default);
+
   return (
     <QueryClientProvider client={queryClient}>
-      <NavigationContainer>
-        <MainScreen />
-      </NavigationContainer>
+      <SafeAreaProvider style={[styles.container, { backgroundColor }]}>
+        <NavigationContainer>
+          <SafeAreaView style={styles.container}>
+            <MainScreen />
+          </SafeAreaView>
+        </NavigationContainer>
+      </SafeAreaProvider>
     </QueryClientProvider>
   );
 };
+
+const styles = StyleSheet.create({ container: { flex: 1 } });
