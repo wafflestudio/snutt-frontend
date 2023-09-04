ef04b8426bfff0debb87649c44e9c3d8ffb3e7ab
Hyunmin Woo (Hanul) <woohm404@gmail.com>
2023-09-05 00:46:44 +0900

    🩹 cat 이미지 png -> svg 변경 (#65)

--- friends/src/app/assets/svg.d.ts
+++ friends/src/app/assets/svg.d.ts
@@ -1,6 +1,8 @@
 declare module '*.svg' {
   import React from 'react';
   import { SvgProps } from 'react-native-svg';
-  const content: React.FC<Omit<SvgProps, 'style'> & { style?: Partial<{ color: string }> }>;
+  const content: React.FC<
+    Omit<SvgProps, 'style'> & { style?: Partial<{ color: string; width: number; height: number }> }
+  >;
   export default content;
 }
--- friends/src/app/components/Icons/SnuttTextIcon.tsx
+++ friends/src/app/components/Icons/SnuttTextIcon.tsx
@@ -1,4 +1,4 @@
-import Icon from '../../assets/snutt-text.svg';
+import Icon from '../../assets/icons/snutt-text.svg';
 import { createIconComponent } from './_createIconComponent';
 
 export const SnuttTextIcon = createIconComponent(Icon);
