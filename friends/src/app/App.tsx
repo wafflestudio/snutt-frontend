cd4a41c18b3b0fa2869139cfc1a35411aaad3aa1
Hyunmin Woo (Hanul) <woohm404@gmail.com>
2023-09-05 10:23:54 +0900

    🩹 navigation container 기본 배경 (#66)

--- friends/src/app/App.tsx
+++ friends/src/app/App.tsx
@@ -1,16 +1,22 @@
 import 'react-native-gesture-handler';
 
-import { NavigationContainer } from '@react-navigation/native';
+import { DefaultTheme, NavigationContainer } from '@react-navigation/native';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { useMemo } from 'react';
 
+import { useThemeContext } from './contexts/ThemeContext';
 import { MainScreen } from './screens/MainScreen';
 
 const queryClient = new QueryClient();
 
 export const App = () => {
+  const background = useThemeContext().color.bg.default;
+
   return (
     <QueryClientProvider client={queryClient}>
-      <NavigationContainer>
+      <NavigationContainer
+        theme={useMemo(() => ({ ...DefaultTheme, colors: { ...DefaultTheme.colors, background } }), [background])}
+      >
         <MainScreen />
       </NavigationContainer>
     </QueryClientProvider>
