f88f308bff864cbaacf78b5861037194f8a06e2d
Hyunmin Woo (Hanul) <woohm404@gmail.com>
2023-09-12 11:56:01 +0900

    👽 시간표 자동조정 로직 클라랑 통일 및 종료시간 버그 수정 (#88)

--- apps/friends-react-native/src/usecases/timetableViewService.ts
+++ apps/friends-react-native/src/usecases/timetableViewService.ts
@@ -4,7 +4,7 @@ import { FullTimetable } from '../entities/timetable';
 
 export type TimetableViewService = {
   getDayRange: (table: Pick<FullTimetable, 'lectures'>) => [Day, Day];
-  getHourRange: (table: Pick<FullTimetable, 'lectures'>) => [Hour, Hour];
+  getHourRange: (table: Pick<FullTimetable, 'lectures'>) => [Hour, Hour]; // Note: [9, 17] 이면 9:00 ~ 17:59 를 의미한다
   getDayLabel: (day: Day) => string;
   getLessonColor: (lesson: FullTimetable['lectures'][number], palette: Color[]) => { bg: string; fg: string };
 };
