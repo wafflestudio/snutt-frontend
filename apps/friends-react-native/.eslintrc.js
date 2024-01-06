2d8fad1d4a5d268460cf24e584c1799e7e43e246
woohm402 <woohm404@gmail.com>
2024-01-06 17:50:53 +0900

    init map-webview

--- apps/friends-react-native/.eslintrc.js
+++ apps/friends-react-native/.eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   root: true,
   extends: '@react-native',
-  plugins: ['simple-import-sort'],
+  plugins: ['simple-import-sort', 'jest'],
   rules: {
     'simple-import-sort/imports': 'error',
     'react/react-in-jsx-scope': 'off',
