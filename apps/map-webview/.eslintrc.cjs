20866b5e14dd51437df417c7aaf7e26e08b88cb0
Hyunmin Woo (Hanul) <woohm404@gmail.com>
2024-01-06 18:57:29 +0900

    🎉 init map-webview (#104)

---
+++ apps/map-webview/.eslintrc.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+  ],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["simple-import-sort"],
+  rules: {
+    "simple-import-sort/imports": "error",
+  },
+};
