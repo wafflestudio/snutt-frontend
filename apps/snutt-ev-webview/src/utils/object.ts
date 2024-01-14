8df986f1eba90a85d301947fb40ad8e86824b587
KiWan KIM <dkwanm00@gmail.com>
2024-01-14 16:09:03 +0900

    snakeToCamel fetchClient에 적용 (#113)

---
+++ apps/snutt-ev-webview/src/utils/object.ts
@@ -0,0 +1,15 @@
+export const snakeToCamel = <T>(obj: object): T => {
+  if (Array.isArray(obj)) {
+    return obj.map((v) => snakeToCamel(v)) as T;
+  }
+
+  if (obj !== null && typeof obj === 'object') {
+    return Object.entries(obj).reduce((acc, [key, value]) => {
+      const camelKey = key.replace(/([-_][a-z])/g, (group) => group.toUpperCase().replace('-', '').replace('_', ''));
+      console.log(camelKey);
+      return { ...acc, [camelKey]: snakeToCamel(value) };
+    }, {}) as T;
+  }
+
+  return obj;
+};
