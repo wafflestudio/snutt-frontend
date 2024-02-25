c727d6774b5ab25101c50336698151a756ed2bdd
ars-ki-00 <dkwanm1@snu.ac.kr>
2024-02-25 15:46:31 +0900

    [메인] 최근 강의목록 carousel 추가

--- apps/snutt-ev-webview/src/app/_pages/MainPage/MainPageRecentSection/index.tsx
+++ apps/snutt-ev-webview/src/app/_pages/MainPage/MainPageRecentSection/index.tsx
@@ -1,17 +1,25 @@
 import Link from 'next/link';
 
+import { CarouselCard } from '@/app/_components/Lecture/Carousel/Card';
 import { Text } from '@/app/_components/Text';
+import { Lecture } from '@/entities/Lecture';
 
-export const MainPageRecentSection = ({ lectures }: { lectures: unknown[] }) => {
+import styles from './index.module.css';
+
+export const MainPageRecentSection = ({ lectures }: { lectures: Lecture[] }) => {
   return (
-    <section>
-      <div>
+    <section className={styles.wrapper}>
+      <div className={styles.header}>
         <Text>지난 학기 강의평을 남겨주세요</Text>
         <Link href="/recent" data-testid="main-recent-more-link">
           <Text>강의 목록 {'>'}</Text>
         </Link>
       </div>
-      <div>{JSON.stringify(lectures)}</div>
+      <div className={styles.carousel}>
+        {lectures.map((lecture) => (
+          <CarouselCard key={lecture.id} lecture={lecture} />
+        ))}
+      </div>
     </section>
   );
 };
