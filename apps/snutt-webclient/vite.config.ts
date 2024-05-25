d046898aed87fa4905689f90af4ee2ae330b34d7
woohm402 <woohm404@gmail.com>
2024-05-25 20:56:49 +0900

    로컬 dev 환경 셋업

--- apps/snutt-webclient/vite.config.ts
+++ apps/snutt-webclient/vite.config.ts
@@ -1,11 +1,24 @@
 import react from '@vitejs/plugin-react-swc';
 import { resolve } from 'path';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: [{ find: '@', replacement: resolve(__dirname, 'src/') }],
-  },
-});
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return defineConfig({
+    plugins: [react()],
+    resolve: {
+      alias: [{ find: '@', replacement: resolve(__dirname, 'src/') }],
+    },
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_BASE_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        },
+      },
+    },
+  });
+};
