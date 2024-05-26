da371495bf7b7d21ec18b16dabafed73ca4491c0
woohm402 <woohm404@gmail.com>
2024-05-26 11:34:22 +0900

    remove unnecessary codes

--- apps/snutt-webclient/src/clients/StorageClient.ts
+++
@@ -1,7 +0,0 @@
-import { type StorageKey } from '@/entities/storage';
-
-export interface StorageClient {
-  get(key: StorageKey): string | null;
-  set(key: StorageKey, value: string): void;
-  remove(key: StorageKey): void;
-}
