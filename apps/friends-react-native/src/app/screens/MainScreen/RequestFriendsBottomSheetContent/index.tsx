987de82d5e3f5ccb03195fc9f1674a9dcaba1c80
양주현 <didwngus01@naver.com>
2024-07-13 19:19:05 +0900

    똑같은 환경 만들어보기

--- apps/friends-react-native/src/app/screens/MainScreen/RequestFriendsBottomSheetContent/index.tsx
+++ apps/friends-react-native/src/app/screens/MainScreen/RequestFriendsBottomSheetContent/index.tsx
@@ -1,4 +1,4 @@
-import { View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import { useMainScreenContext } from '..';
 import { BottomSheet } from '../../../components/BottomSheet';
 import { RequestFriendsMethodList } from './RequestFriendsMethodList';
@@ -10,10 +10,17 @@ export const RequestFriendsBottomSheetContent = () => {
   const closeAddFriendModal = () => dispatch({ type: 'setRequestFriendModalOpen', isOpen: false });
 
   return (
-    <View>
-      <BottomSheet isOpen={isRequestFriendModalOpen} onClose={closeAddFriendModal}>
-        {requestFriendModalStep === 'METHOD_LIST' ? <RequestFriendsMethodList /> : <RequestFriendsWithNickname />}
-      </BottomSheet>
+    <View style={styles.container}>
+      <View style={styles.tabContent}>
+        <BottomSheet isOpen={isRequestFriendModalOpen} onClose={closeAddFriendModal}>
+          {requestFriendModalStep === 'METHOD_LIST' ? <RequestFriendsMethodList /> : <RequestFriendsWithNickname />}
+        </BottomSheet>
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: { paddingVertical: 22, height: '100%', display: 'flex' },
+  tabContent: { paddingTop: 16, flex: 1 },
+});
