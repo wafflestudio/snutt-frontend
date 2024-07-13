d98cab34f29245b7df3a9c36d0c3da5151c32eaa
양주현 <didwngus01@naver.com>
2024-07-13 19:48:53 +0900

    그럴 리 없지

--- apps/friends-react-native/src/app/components/Icons/Kakaotalk.tsx
+++ apps/friends-react-native/src/app/components/Icons/Kakaotalk.tsx
@@ -1,4 +1,4 @@
-import Icon from '../../assets/icons/pencil.svg';
+import Icon from '../../assets/icons/kakaotalk.svg';
 import { createIconComponent } from './_createIconComponent';
 
 export const KakaotalkIcon = createIconComponent(Icon);
