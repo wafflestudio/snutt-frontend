b37f67ec89f19918ea05d491803893f97af07367
ars-ki-00 <dkwanm1@snu.ac.kr>
2024-09-16 17:11:00 +0900

    [theme-market] 내 테마 올리기 => 테마 클릭 시 바텀시트 추가

--- apps/theme-market/src/app/(routes)/(main)/@bottomsheet/(.)theme/[id]/page.tsx
+++ apps/theme-market/src/app/(routes)/(main)/@bottomsheet/(.)theme/[id]/page.tsx
@@ -1,5 +1,5 @@
 import { BottomSheet } from "@/app/_components/BottomSheet";
-import { Theme } from "@/app/_pages/Main/ThemeDownload/BottomSheet/Theme";
+import { Theme } from "@/app/_pages/Main/BottomSheet/Theme";
 
 export default function Page() {
   return (
--- apps/theme-market/src/app/_pages/Main/MyTheme/ThemeList/Theme/index.tsx
+++ apps/theme-market/src/app/_pages/Main/MyTheme/ThemeList/Theme/index.tsx
@@ -1,3 +1,7 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
 import styles from "./index.module.css";
 
 interface Props {
@@ -6,8 +10,13 @@ interface Props {
 }
 
 export const Theme = ({ title }: Props) => {
+  const router = useRouter();
+
   return (
-    <article className={styles.wrapper}>
+    <article
+      className={styles.wrapper}
+      onClick={() => router.push("/theme/123")}
+    >
       <div className={styles.preview} />
       <span className={styles.title}>{title}</span>
     </article>
