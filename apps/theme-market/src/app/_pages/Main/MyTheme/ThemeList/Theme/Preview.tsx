01e4cd413e99c70baddb02a2b3d605374f808d65
ars-ki-00 <dkwanm1@snu.ac.kr>
2024-09-16 17:59:22 +0900

    [theme-market] 내 테마 올리기 => 내 커스텀 테마 프리뷰 추가

---
+++ apps/theme-market/src/app/_pages/Main/MyTheme/ThemeList/Theme/Preview.tsx
@@ -0,0 +1,364 @@
+"use client";
+
+import classNames from "classnames";
+import styles from "./index.module.css";
+
+interface Props {
+  colors: string[];
+}
+
+export const Preview = ({ colors }: Props) => {
+  const colorsCount = colors.length;
+
+  return (
+    <div className={styles.preview}>
+      {(() => {
+        switch (colorsCount) {
+          case 1:
+            return (
+              <div
+                className={classNames(styles.w_full, styles.h_full)}
+                style={{ backgroundColor: `${colors[0]}` }}
+              />
+            );
+          case 2:
+            return (
+              <>
+                <div
+                  className={classNames(styles.w_half, styles.h_full)}
+                  style={{ backgroundColor: `${colors[0]}` }}
+                />
+                <div
+                  className={classNames(styles.w_half, styles.h_full)}
+                  style={{ backgroundColor: `${colors[1]}` }}
+                />
+              </>
+            );
+          case 3:
+            return (
+              <>
+                <div
+                  className={classNames(styles.w_27, styles.h_full)}
+                  style={{ backgroundColor: `${colors[0]}` }}
+                />
+                <div
+                  className={classNames(styles.w_27, styles.h_full)}
+                  style={{ backgroundColor: `${colors[1]}` }}
+                />
+                <div
+                  className={classNames(styles.w_27, styles.h_full)}
+                  style={{ backgroundColor: `${colors[2]}` }}
+                />
+              </>
+            );
+          case 4:
+            return (
+              <>
+                <div
+                  className={classNames(styles.w_27, styles.h_full)}
+                  style={{ backgroundColor: `${colors[0]}` }}
+                />
+                <div
+                  className={classNames(
+                    styles.w_27,
+                    styles.h_full,
+                    styles.flex_c
+                  )}
+                >
+                  <div
+                    className={classNames(styles.w_27, styles.h_half)}
+                    style={{ backgroundColor: `${colors[1]}` }}
+                  />
+                  <div
+                    className={classNames(styles.w_27, styles.h_half)}
+                    style={{ backgroundColor: `${colors[2]}` }}
+                  />
+                </div>
+
+                <div
+                  className={classNames(styles.w_27, styles.h_full)}
+                  style={{ backgroundColor: `${colors[3]}` }}
+                />
+              </>
+            );
+          case 5:
+            return (
+              <>
+                <div
+                  className={classNames(
+                    styles.w_27,
+                    styles.h_full,
+                    styles.flex_c
+                  )}
+                >
+                  <div
+                    className={classNames(styles.w_27, styles.h_half)}
+                    style={{ backgroundColor: `${colors[0]}` }}
+                  />
+                  <div
+                    className={classNames(styles.w_27, styles.h_half)}
+                    style={{ backgroundColor: `${colors[1]}` }}
+                  />
+                </div>
+                <div
+                  className={classNames(styles.w_27, styles.h_full)}
+                  style={{ backgroundColor: `${colors[2]}` }}
+                />
+                <div
+                  className={classNames(
+                    styles.w_27,
+                    styles.h_full,
+                    styles.flex_c
+                  )}
+                >
+                  <div
+                    className={classNames(styles.w_27, styles.h_half)}
+                    style={{ backgroundColor: `${colors[3]}` }}
+                  />
+                  <div
+                    className={classNames(styles.w_27, styles.h_half)}
+                    style={{ backgroundColor: `${colors[4]}` }}
+                  />
+                </div>
+              </>
+            );
+          case 6:
+            return (
+              <>
+                <div
+                  className={classNames(styles.w_20, styles.h_full)}
+                  style={{ backgroundColor: `${colors[0]}` }}
+                />
+                <div
+                  className={classNames(
+                    styles.w_20,
+                    styles.h_full,
+                    styles.flex_c
+                  )}
+                >
+                  <div
+                    className={classNames(styles.w_full, styles.h_half)}
+                    style={{ backgroundColor: `${colors[1]}` }}
+                  />
+                  <div
+                    className={classNames(styles.w_full, styles.h_half)}
+                    style={{ backgroundColor: `${colors[2]}` }}
+                  />
+                </div>
+                <div
+                  className={classNames(
+                    styles.w_20,
+                    styles.h_full,
+                    styles.flex_c
+                  )}
+                >
+                  <div
+                    className={classNames(styles.w_full, styles.h_half)}
+                    style={{ backgroundColor: `${colors[3]}` }}
+                  />
+                  <div
+                    className={classNames(styles.w_full, styles.h_half)}
+                    style={{ backgroundColor: `${colors[4]}` }}
+                  />
+                </div>
+                <div
+                  className={classNames(styles.w_20, styles.h_full)}
+                  style={{ backgroundColor: `${colors[5]}` }}
+                />
+              </>
+            );
+          case 7:
+            return (
+              <>
+                <div
+                  className={classNames(
+                    styles.w_27,
+                    styles.h_full,
+                    styles.flex_c
+                  )}
+                >
+                  <div
+                    className={classNames(styles.w_full, styles.h_half)}
+                    style={{ backgroundColor: `${colors[0]}` }}
+                  />
+                  <div
+                    className={classNames(styles.w_full, styles.h_half)}
+                    style={{ backgroundColor: `${colors[1]}` }}
+                  />
+                </div>
+                <div
+                  className={classNames(
+                    styles.w_27,
+                    styles.h_full,
+                    styles.flex_c
+                  )}
+                >
+                  <div
+                    className={classNames(styles.w_full, styles.h_27)}
+                    style={{ backgroundColor: `${colors[2]}` }}
+                  />
+                  <div
+                    className={classNames(styles.w_full, styles.h_27)}
+                    style={{ backgroundColor: `${colors[3]}` }}
+                  />
+                  <div
+                    className={classNames(styles.w_full, styles.h_27)}
+                    style={{ backgroundColor: `${colors[4]}` }}
+                  />
+                </div>
+                <div
+                  className={classNames(
+                    styles.w_27,
+                    styles.h_full,
+                    styles.flex_c
+                  )}
+                >
+                  <div
+                    className={classNames(styles.w_full, styles.h_half)}
+                    style={{ backgroundColor: `${colors[5]}` }}
+                  />
+                  <div
+                    className={classNames(styles.w_full, styles.h_half)}
+                    style={{ backgroundColor: `${colors[6]}` }}
+                  />
+                </div>
+              </>
+            );
+          case 8:
+            return (
+              <>
+                <div
+                  className={classNames(
+                    styles.w_27,
+                    styles.h_full,
+                    styles.flex_c
+                  )}
+                >
+                  <div
+                    className={classNames(styles.w_full, styles.h_27)}
+                    style={{ backgroundColor: `${colors[0]}` }}
+                  />
+                  <div
+                    className={classNames(styles.w_full, styles.h_27)}
+                    style={{ backgroundColor: `${colors[1]}` }}
+                  />
+                  <div
+                    className={classNames(styles.w_full, styles.h_27)}
+                    style={{ backgroundColor: `${colors[2]}` }}
+                  />
+                </div>
+                <div
+                  className={classNames(
+                    styles.w_27,
+                    styles.h_full,
+                    styles.flex_c
+                  )}
+                >
+                  <div
+                    className={classNames(styles.w_full, styles.h_half)}
+                    style={{ backgroundColor: `${colors[3]}` }}
+                  />
+                  <div
+                    className={classNames(styles.w_full, styles.h_half)}
+                    style={{ backgroundColor: `${colors[4]}` }}
+                  />
+                </div>
+                <div
+                  className={classNames(
+                    styles.w_27,
+                    styles.h_full,
+                    styles.flex_c
+                  )}
+                >
+                  <div
+                    className={classNames(styles.w_full, styles.h_27)}
+                    style={{ backgroundColor: `${colors[5]}` }}
+                  />
+                  <div
+                    className={classNames(styles.w_full, styles.h_27)}
+                    style={{ backgroundColor: `${colors[6]}` }}
+                  />
+                  <div
+                    className={classNames(styles.w_full, styles.h_27)}
+                    style={{ backgroundColor: `${colors[7]}` }}
+                  />
+                </div>
+              </>
+            );
+          case 9:
+            return (
+              <>
+                <div
+                  className={classNames(styles.w_20, styles.h_full)}
+                  style={{ backgroundColor: `${colors[0]}` }}
+                />
+                <div
+                  className={classNames(
+                    styles.w_20,
+                    styles.h_full,
+                    styles.flex_c
+                  )}
+                >
+                  <div
+                    className={classNames(styles.w_full, styles.h_26)}
+                    style={{ backgroundColor: `${colors[1]}` }}
+                  />
+                  <div
+                    className={classNames(styles.w_full, styles.h_28)}
+                    style={{ backgroundColor: `${colors[2]}` }}
+                  />
+                  <div
+                    className={classNames(styles.w_full, styles.h_24)}
+                    style={{ backgroundColor: `${colors[3]}` }}
+                  />
+                </div>
+                <div
+                  className={classNames(
+                    styles.w_20,
+                    styles.h_full,
+                    styles.flex_c
+                  )}
+                >
+                  <div
+                    className={classNames(styles.w_full, styles.h_18)}
+                    style={{ backgroundColor: `${colors[4]}` }}
+                  />
+                  <div
+                    className={classNames(styles.w_full, styles.h_27)}
+                    style={{ backgroundColor: `${colors[5]}` }}
+                  />
+                  <div
+                    className={classNames(styles.w_full, styles.h_33)}
+                    style={{ backgroundColor: `${colors[6]}` }}
+                  />
+                </div>
+                <div
+                  className={classNames(
+                    styles.w_20,
+                    styles.h_full,
+                    styles.flex_c
+                  )}
+                >
+                  <div
+                    className={classNames(styles.w_full, styles.h_half)}
+                    style={{ backgroundColor: `${colors[7]}` }}
+                  />
+                  <div
+                    className={classNames(styles.w_full, styles.h_half)}
+                    style={{ backgroundColor: `${colors[8]}` }}
+                  />
+                </div>
+              </>
+            );
+          default:
+            return (
+              <div
+                className={classNames(styles.w_full, styles.h_full)}
+                style={{ backgroundColor: `${colors[0]}` }}
+              />
+            );
+        }
+      })()}
+    </div>
+  );
+};
--- apps/theme-market/src/app/_pages/Main/MyTheme/ThemeList/index.tsx
+++ apps/theme-market/src/app/_pages/Main/MyTheme/ThemeList/index.tsx
@@ -4,10 +4,64 @@ import { Theme } from "./Theme";
 export const ThemeList = () => {
   return (
     <div className={styles.wrapper}>
-      <Theme title="새 테마" colors={[]} />
-      <Theme title="새 테마" colors={[]} />
-      <Theme title="새 테마" colors={[]} />
-      <Theme title="새 테마" colors={[]} />
+      <Theme title="1" colors={["#1BD0C8"]} />
+      <Theme title="2" colors={["#1BD0C8", "#FAC42D"]} />
+      <Theme title="3" colors={["#1BD0C8", "#FAC42D", "#E54459"]} />
+      <Theme title="4" colors={["#1BD0C8", "#FAC42D", "#A6D930", "#E54459"]} />
+      <Theme
+        title="5"
+        colors={["#1BD0C8", "#FAC42D", "#A6D930", "#E54459", "#4F48C4"]}
+      />
+      <Theme
+        title="6"
+        colors={[
+          "#1BD0C8",
+          "#FAC42D",
+          "#A6D930",
+          "#E54459",
+          "#4F48C4",
+          "#AF56B3",
+        ]}
+      />
+      <Theme
+        title="7"
+        colors={[
+          "#1BD0C8",
+          "#FAC42D",
+          "#A6D930",
+          "#E54459",
+          "#4F48C4",
+          "#AF56B3",
+          "#F58D3D",
+        ]}
+      />
+      <Theme
+        title="8"
+        colors={[
+          "#1BD0C8",
+          "#FAC42D",
+          "#A6D930",
+          "#E54459",
+          "#4F48C4",
+          "#AF56B3",
+          "#F58D3D",
+          "#1D99E8",
+        ]}
+      />
+      <Theme
+        title="9"
+        colors={[
+          "#1BD0C8",
+          "#FAC42D",
+          "#A6D930",
+          "#E54459",
+          "#4F48C4",
+          "#AF56B3",
+          "#F58D3D",
+          "#1D99E8",
+          "#123456",
+        ]}
+      />
     </div>
   );
 };
