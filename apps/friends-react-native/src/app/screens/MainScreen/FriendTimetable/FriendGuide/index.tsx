2063ed3523da4e6d1d83959239aa1b79b0f76b21
ars-ki-00 <dkwanm1@snu.ac.kr>
2025-03-08 15:57:03 +0900

    [friends-native] 망한 시간표 대회 - 텍스트 수정

--- apps/friends-react-native/src/app/screens/MainScreen/FriendTimetable/FriendGuide/index.tsx
+++ apps/friends-react-native/src/app/screens/MainScreen/FriendTimetable/FriendGuide/index.tsx
@@ -1,13 +1,14 @@
 import { StyleSheet, TouchableOpacity } from 'react-native';
 
 import { EmptyView } from '../../../../components/EmptyView';
-import { HamburgerIcon } from '../../../../components/Icons/HamburgerIcon';
+// import { HamburgerIcon } from '../../../../components/Icons/HamburgerIcon';
 import { QuestionIcon } from '../../../../components/Icons/QuestionIcon';
 import { UserPlusIcon } from '../../../../components/Icons/UserPlusIcon';
 import { Paper } from '../../../../components/Paper';
 import { Typography } from '../../../../components/Typography';
 import { useThemeContext } from '../../../../contexts/ThemeContext';
 import { useMainScreenContext } from '../..';
+import { CheckIcon } from '../../../../components/Icons/CheckIcon';
 
 export const FriendGuide = () => {
   const { dispatch } = useMainScreenContext();
@@ -23,17 +24,40 @@ export const FriendGuide = () => {
     <Paper style={styles.wrapper}>
       <EmptyView
         size="big"
-        title="추가한 친구가 없습니다."
+        title="망한 시간표 대회 참여 방법"
         descriptions={[
           <>
-            우측 상단 <UserPlusIcon style={{ color: caption }} width={12} height={12} /> 을 눌러
+            <CheckIcon style={{ color: caption }} width={24} height={36} /> 우측 상단{' '}
+            <UserPlusIcon style={{ color: caption }} width={12} height={12} /> 을 눌러
+            <Typography style={styles.bold}>'수신망한와플#7777'</Typography>로
           </>,
-          '시간표를 공유하고 싶은 친구에게 친구 요청을 보내보세요!',
+          '친구 요청을 보내주시면 자동 신청됩니다.',
           <>
-            친구가 수락하면 사이드바 <HamburgerIcon style={{ color: caption }} width={12} height={12} /> 친구 목록에
-            추가됩니다.
+            <CheckIcon style={{ color: caption }} width={24} height={36} /> 2025년 1학기 시간표 중 하나가
           </>,
+          <>
+            <Typography style={styles.bold}>'대표 시간표'</Typography>로 지정되어 있는지 확인해주세요.
+          </>,
+          <>
+            <CheckIcon style={{ color: caption }} width={24} height={36} /> 발표(3/30) 전에 친구 삭제 시 무효
+            처리됩니다.
+          </>,
+          // <>
+          //   친구가 수락하면 사이드바 <HamburgerIcon style={{ color: caption }} width={12} height={12} /> 친구 목록에
+          //   추가됩니다.
+          // </>,
         ]}
+        // title="추가한 친구가 없습니다."
+        // descriptions={[
+        //   <>
+        //     우측 상단 <UserPlusIcon style={{ color: caption }} width={12} height={12} /> 을 눌러
+        //   </>,
+        //   '시간표를 공유하고 싶은 친구에게 친구 요청을 보내보세요!',
+        //   <>
+        //     친구가 수락하면 사이드바 <HamburgerIcon style={{ color: caption }} width={12} height={12} /> 친구 목록에
+        //     추가됩니다.
+        //   </>,
+        // ]}
         etc={
           <TouchableOpacity onPress={openGuideModal} style={styles.detailButton}>
             <QuestionIcon style={{ color: hint }} width={12} height={12} />
@@ -52,4 +76,5 @@ const styles = StyleSheet.create({
   typoMargin4: { paddingTop: 8 },
   detailButton: { marginTop: 20, flexDirection: 'row', alignItems: 'center', gap: 4 },
   hint: { textDecorationLine: 'underline', fontSize: 10 },
+  bold: { fontWeight: 'bold' },
 });
