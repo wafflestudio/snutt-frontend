915cc33d6d1ff873dc1eb74cd858a5032749bf56
ars-ki-00 <dkwanm1@snu.ac.kr>
2025-03-16 15:49:43 +0900

    [theme-market] 뒤로가기시 모달 닫기

--- apps/theme-market/src/app/(routes)/(main)/(modal)/index.tsx
+++ apps/theme-market/src/app/(routes)/(main)/(modal)/index.tsx
@@ -1,10 +1,22 @@
 "use client";
 
-import { Modal } from "@/app/_components/Modal";
+import { useEffect } from "react";
+import { usePathname } from "next/navigation";
+
 import { useModalStore } from "@/app/_providers/ModalProvider";
 
+import { Modal } from "@/app/_components/Modal";
+
 export default function MainModal() {
+  const pathname = usePathname();
+
   const modalStore = useModalStore((state) => state);
+
+  const { closeModal } = modalStore;
+
+  useEffect(() => {
+    closeModal();
+  }, [pathname, closeModal]);
 
   if (!modalStore.isOpen) return null;
 
